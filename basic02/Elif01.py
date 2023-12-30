c767b85c3f9b3632ec6297ab5a77744aba34e504
panand11 <panand.sap@gmail.com>
2023-12-30 15:42:29 +0530

    added else stmnt in prog

--- basic02/Elif01.py
+++ basic02/Elif01.py
@@ -1,12 +1,14 @@
 #elif stmnt works just like else if in C
 
-def login(choice):
+def login(choice):               #def keyword is used to define functions in python
     if choice == 1:
         print("python")
     elif choice == 2:
         print("java")    
     elif choice == 3:
         print("HTML")
+    else:
+        print("invalid choice given")
 
 var = int(input("enter your choice"))
 login(var)
